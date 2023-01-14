208883fb845d3879c957c1cf2595ef90a0f7ce95
Andrei <itunereg@gmail.com>
2023-01-14 11:34:19 +0300

    return back link with promo and solve quiz and get code method

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,11 @@
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
-#   product_page.solve_quiz_and_get_code()
+    product_page.solve_quiz_and_get_code()
     product_page.book_name_in_basket()
     product_page.book_price_in_basket()
 
