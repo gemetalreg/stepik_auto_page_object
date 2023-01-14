7403baac4bd84577fcff435bc630f0b5f5c23ee4
Andrei <itunereg@gmail.com>
2023-01-14 11:29:52 +0300

    fix product page with text attr

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,10 @@
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
 class BasePage():
-    def __init__(self, browser, url) -> None:
+    def __init__(self, browser, url, timeout=10) -> None:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(10)
 
     def open(self):
         self.browser.get(self.url)
--- pages/product_page.py
+++ pages/product_page.py
@@ -5,3 +5,16 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_LINK)
         add_to_basket_btn.click()
+
+    def book_name_in_basket(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+        book_name_in_msg = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_MESSAGE)
+        assert book_name.text == book_name_in_msg.text, "Book name don't equal to book name in basket"
+        print(f"{book_name} is right")
+
+    def book_price_in_basket(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        book_price_in_msg = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_IN_MESSAGE)
+        print(book_price, book_price_in_msg)
+        assert book_price.text == book_price_in_msg.text, "Book price don't equal to book price in basket"
+        print(f"Book price {book_price} is right")
