e772f22640f27e9983e0db7b50197fe5405dc317
Andrei <itunereg@gmail.com>
2023-01-14 11:47:11 +0300

    add get methods to productpage

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,15 +6,20 @@ class ProductPage(BasePage):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_LINK)
         add_to_basket_btn.click()
 
-    def book_name_in_basket(self):
-        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+    def book_name_in_basket(self, book_name):
         book_name_in_msg = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_MESSAGE)
         assert book_name.text == book_name_in_msg.text, "Book name don't equal to book name in basket"
         print(f"{book_name} is right")
 
-    def book_price_in_basket(self):
-        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+    def book_price_in_basket(self, book_price):
         book_price_in_msg = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_IN_MESSAGE)
-        print(book_price, book_price_in_msg)
         assert book_price.text == book_price_in_msg.text, "Book price don't equal to book price in basket"
         print(f"Book price {book_price} is right")
+
+    def get_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+        return book_name
+
+    def get_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        return book_price
