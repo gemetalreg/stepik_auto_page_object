b004bae98f07eaf4c848d3596170441bfabe547d
Andrei <itunereg@gmail.com>
2023-01-16 08:09:45 +0300

    add is_not_element_present and is_disappeared in base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import math
 class BasePage():
     def __init__(self, browser, url, timeout=10) -> None:
@@ -8,6 +11,23 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, (TimeoutException)).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
