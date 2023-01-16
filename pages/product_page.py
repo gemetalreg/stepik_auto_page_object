171d3bcb41551f2a846a41e4e0f8f374025a3bf6
Andrei <itunereg@gmail.com>
2023-01-16 08:10:51 +0300

    add should_not_be_success_message and should_be_sucecess_message_disappiare in [prodcut page

--- pages/product_page.py
+++ pages/product_page.py
@@ -23,3 +23,12 @@ class ProductPage(BasePage):
     def get_book_price(self):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
         return book_price
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should not be"
+
+    def should_be_sucecess_message_disappiare(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message isn't disappiare, but should be"
+
