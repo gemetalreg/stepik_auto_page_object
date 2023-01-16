aedd11304e02cbfc0b051c49124b66dc678a88ee
Andrei <itunereg@gmail.com>
2023-01-16 08:46:00 +0300

    use timeout param in basepage init to implicitly wait method

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,7 +7,7 @@ class BasePage():
     def __init__(self, browser, url, timeout=10) -> None:
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
