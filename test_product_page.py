82dcc1f767323cba2f13da47f5377a310b58658c
Andrei <itunereg@gmail.com>
2023-01-17 09:48:13 +0300

    fix bug in test product page

--- test_product_page.py
+++ test_product_page.py
@@ -48,7 +48,7 @@ class TestUserAddToBasketFromProductPage():
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_to_basket()
+    page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.book_name_in_basket(page.get_book_name())
     page.book_price_in_basket(page.get_book_price())
@@ -76,7 +76,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
